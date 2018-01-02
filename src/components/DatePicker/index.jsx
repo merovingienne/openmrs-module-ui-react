28eac55ad6c7ff577797b1d456eb66d08a7ace40
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-01-02 22:56:41 +0100

    GCI-265 React UI Library - Date Picker component

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -1,9 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import RangeCalendar from "rc-calendar/lib/RangeCalendar";
+import Calendar from "rc-calendar/lib/Calendar";
+import TimePicker from 'rc-time-picker';
+import PropTypes from 'prop-types';
+
+import "rc-calendar/dist/rc-calendar.css"
+import "rc-time-picker/assets/index.css"
+import './datepicker.css'
+
 
 class DatePicker extends Component {
-    render(){
-        return(<div>Datepicker - WIP</div>);
+
+    shouldBlockDate(date) {
+        if (this.props.blockFrom === undefined || this.props.blockTo === undefined || date === undefined) return false;
+
+        return date.isBetween(this.props.blockFrom, this.props.blockTo, 'days', '[]')
+    };
+
+    render() {
+        if (this.props.ranged && this.props.withTime) {
+            return <RangeCalendar
+                defaultValue={this.props.defaultDate}
+                disabledDate={(date) => this.shouldBlockDate(date)}
+                onChange={this.props.onSelect}
+                timePicker={<TimePicker/>}
+            />;
+        } else if (this.props.ranged) {
+            return <RangeCalendar
+                defaultValue={this.props.defaultDate}
+                disabledDate={(date) => this.shouldBlockDate(date)}
+                onChange={this.props.onSelect}
+            />;
+        } else if (this.props.withTime) {
+            return <Calendar
+                defaultValue={this.props.defaultDate}
+                disabledDate={(date) => this.shouldBlockDate(date)}
+                onChange={this.props.onSelect}
+                timePicker={<TimePicker/>}
+            />;
+        } else {
+            return <Calendar
+                defaultValue={this.props.defaultDate}
+                disabledDate={(date) => this.shouldBlockDate(date)}
+                onChange={this.props.onSelect}
+            />;
+        }
+
     }
 }
+
+DatePicker.propTypes = {
+    blockFrom: PropTypes.object,
+    blockTo: PropTypes.object,
+    ranged: PropTypes.bool,
+    withTime: PropTypes.bool
+};
+
+DatePicker.defaultProps = {
+    blockFrom: undefined,
+    blockTo: undefined,
+    ranged: false,
+    withTime: false
+};
 
 export default DatePicker;
